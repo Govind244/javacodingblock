21d8d1ed533f870050fe77c9ae37ada4ba1134d1
Govind Saraswat <108930066+Govind244@users.noreply.github.com>
2023-06-19 00:11:58 +0530

    Create 2d_array_wave_print_column_wise.java

---
+++ 2d_array_wave_print_column_wise.java
@@ -0,0 +1,39 @@
+/*
+Sample Input
+4 4
+11 12 13 14
+21 22 23 24
+31 32 33 34
+41 42 43 44
+Sample Output
+11, 21, 31, 41, 42, 32, 22, 12, 13, 23, 33, 43, 44, 34, 24, 14, END
+*/
+
+import java.util.*;
+public class Program
+{
+    public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+        int r = sc.nextInt();   // no. of rows
+        int c = sc.nextInt();       // no. of columns
+        int arr[][] = new int[r][c];
+        for(int i=0;i<r;i++)
+        for(int j=0;j<c;j++)
+        arr[i][j]= sc.nextInt();
+
+        for(int i=0;i<c;i++){
+            if(i%2==0){
+               for(int j=0;j<r;j++){
+                System.out.print(arr[j][i] + ", ");
+                }
+            }
+            else{
+                for(int j=r-1;j>=0;j--){
+                System.out.print(arr[j][i] + ", ");
+                }
+            }
+        }    
+        System.out.print("END");
+        }
+	}
+
